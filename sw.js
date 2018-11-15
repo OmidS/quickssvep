4bf1a0ef5c69bcce03132812dece5a54c73d59fe
Omid Sani <OmidS@users.noreply.github.com>
2018-11-14 16:21:55 -0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@ if (workbox) {
   
   workbox.routing.registerRoute(
     // Cache static files
-    '/',
+    './',
     // Use cache but update in the background ASAP
     workbox.strategies.networkFirst({
       // Use a custom cache name
