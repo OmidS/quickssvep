be47e2bc8336b73880a87cf92c228c9eea848617
OmidS <ghasemsa@usc.edu>
2018-11-14 16:14:10 -0800

    adds sw for offline support

---
+++ sw.js
@@ -0,0 +1,53 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox) {
+  console.log(`Workbox is loaded.`);
+  
+  workbox.routing.registerRoute(
+    // Cache static files
+    '/',
+    // Use cache but update in the background ASAP
+    workbox.strategies.networkFirst({
+      // Use a custom cache name
+      cacheName: 'fresh-resources',
+    })
+  );
+
+  workbox.routing.registerRoute(
+    // Cache static files
+    /.*\.(?:htm|html|js|css)$/,
+    // Use cache but update in the background ASAP
+    workbox.strategies.staleWhileRevalidate({
+      // Use a custom cache name
+      cacheName: 'static-resources',
+    })
+  );
+  
+  workbox.routing.registerRoute(
+    // Cache image files
+    /.*\.(?:png|jpg|jpeg|svg|gif|ico|woff|woff2)/,
+    // Use the cache if it's available
+    workbox.strategies.cacheFirst({
+      // Use a custom cache name
+      cacheName: 'image-cache',
+      plugins: [
+        new workbox.expiration.Plugin({
+          // Cache only 20 images
+          maxEntries: 20,
+          // Cache for a maximum of a week
+          maxAgeSeconds: 7 * 24 * 60 * 60,
+        })
+      ],
+    })
+  );
+  
+  // Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+  workbox.routing.registerRoute(
+    /^https:\/\/fonts\.googleapis\.com/,
+    workbox.strategies.staleWhileRevalidate({
+      cacheName: 'google-fonts-stylesheets',
+    })
+  );
+} else {
+  console.log(`Workbox didn't load!`);
+}
